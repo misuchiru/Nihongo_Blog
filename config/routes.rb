d3c57185db6bbe23d54ce27444fba7db1d7f6c4e
misuchiru <perryhaha@gmail.com>
2016-09-23 18:04:50 +0800

    建立後台cates

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :cates
   resources :posts
+  namespace :dashboard do
+    resources :posts
+    resources :cates
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
