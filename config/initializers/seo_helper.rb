f47f11159da3ca87504fc4af5e5891a0c5a6d791
misuchiru <perryhaha@gmail.com>
2016-11-02 17:51:27 +0800

    修改description顯示

--- config/initializers/seo_helper.rb
+++ config/initializers/seo_helper.rb
@@ -1,5 +1,5 @@
 SeoHelper.configure do |config|
   config.skip_blank = false
   config.site_name = "PerryHaHaのBlog"
-  config.default_page_description = "PerryHaHaのブログ"
+  config.default_page_description = "分享學習日語的心得及日本旅遊的行程"
 end
