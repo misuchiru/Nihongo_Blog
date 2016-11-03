639316b2eaaa3365fcf0bea654b8db8689d9cdb9
misuchiru <perryhaha@gmail.com>
2016-11-03 16:47:43 +0800

    sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,34 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://perryhaha.com"
+
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+  Post.find_each do |post|
+    add post_path(post), lastmod: post.updated_at
+  end
+end
+
+SitemapGenerator::Sitemap.ping_search_engines
