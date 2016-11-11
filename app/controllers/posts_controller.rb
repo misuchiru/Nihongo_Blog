11f4d40d66892b922d4fdf2f68079c5a57ecf002
misuchiru <perryhaha@gmail.com>
2016-11-11 12:54:33 +0800

    N + 1 查詢的解法 .includes(:cate)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,10 @@ class PostsController < ApplicationController
 
   def index
     if params[:cate].blank?
-      @posts = @paginate = Post.published.order('id DESC').paginate(:page => params[:page])
+      @posts = @paginate = Post.includes(:cate).published.order('id DESC').paginate(:page => params[:page])
     else
       @cate_id = Cate.find_by(name: params[:cate]).id
-      @posts = @paginate = Post.published.where(cate_id: @cate_id).order('id DESC').paginate(:page => params[:page])
+      @posts = @paginate = Post.includes(:cate).published.where(cate_id: @cate_id).order('id DESC').paginate(:page => params[:page])
     end
 
   end
